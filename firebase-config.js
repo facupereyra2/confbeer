4f7240493d5eba695d709167ee03fc96e72e8026
Facundo Pereyra <63249412+facupereyra2@users.noreply.github.com>
2022-11-16 18:32:51 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyB-Z4aePhW89r6jGzWcp8cE5Zd2OpHkRqo",
-  authDomain: "confbeer-5a512.firebaseapp.com",
-  projectId: "confbeer-5a512",
-  storageBucket: "confbeer-5a512.appspot.com",
-  messagingSenderId: "769878642829",
-  appId: "1:769878642829:web:96296be9afac51159e52c4"
+  apiKey: ,
+  authDomain:,
+  projectId: ,
+  storageBucket: ,
+  messagingSenderId:,
+  appId: 
 };
 
 
